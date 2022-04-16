db755b6e7fb777a210066fd269ff8eb6486d66bb
Prashil Silwal <prashil_silwal@protonmail.com>
2022-04-16 07:56:20 +0545

    Updated Readme

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'confirmPass',
     ];
 
     /**
@@ -33,7 +32,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'confirmPass',
         'activeStatus'
     ];
 
