1d9500616f7e6ab91940a7193a33e9cc1da4b19e
Prashil Silwal <prashil_silwal@protonmail.com>
2022-05-21 15:17:12 +0545

    re-added user update status of bishwa

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -92,4 +92,27 @@ class AdminController extends Controller
         ], 200);
     }
 
+    public function updateUserStatus(Request $request)
+    {
+        $user = User::where('id', $request->id)->first();
+        try {
+            if (!$user->activeStatus) {
+                $user->activeStatus = true;
+            } else {
+                $user->activeStatus = false;
+            }
+            $user->save();
+            $result = [
+                'status' => 200,
+                'message' => 'User status updated'
+            ];
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+
 }
