145bd8f4df0d8843752bcda0fc92bd6ad7a43884
Pranish97 <pranishstha4@gmail.com>
2022-10-17 10:06:54 +0545

    PTTB-179 Adding perfiled email while reseting password

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         ResetPassword::createUrlUsing(function ($user, string $token) {
-            return config('frontend.url') . '/reset-password?token=' . $token;
+            return config('frontend.url') . '/reset-password?token=' . $token. '&email='.$user->email;
         });
     }
 }
