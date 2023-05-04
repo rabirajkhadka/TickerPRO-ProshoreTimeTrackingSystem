55718adad520617b213f79372d28accf3c64a3a2
piyushmhrzn <piyushmaharjan22@gmail.com>
2023-05-04 15:20:53 +0545

    Client number accepts 10 digit mobile number

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -25,9 +25,17 @@ class ClientRequest extends FormRequest
     {
         return [
             'client_name' => 'required|regex:/^[\pL\s\-]+$/u|max:255',
-            'client_number' => 'required|integer|max:255',
+            'client_number' => 'required|numeric|digits:10|regex:/^\d{10}$/',
             'client_email' => 'required | email |max:255|unique:clients',
             'status' => 'required | boolean',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'client_number.numeric' => 'The client number must only contain numbers',
+            'client_number.digits' => 'The client number must be a 10 digit mobile number',
         ];
     }
 }
