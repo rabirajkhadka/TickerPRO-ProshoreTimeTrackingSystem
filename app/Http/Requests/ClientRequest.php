e71a5817e53634535ca80199d357c84ed0ec15bf
piyushmhrzn <piyushmaharjan22@gmail.com>
2023-05-09 13:51:52 +0545

    Added regex comments

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -24,8 +24,8 @@ class ClientRequest extends FormRequest
     public function rules()
     {
         return [
-            'client_name' => 'required|regex:/^[\pL\s\-]+$/u|max:255',
-            'client_number' => 'required|numeric|digits:10|regex:/^\d{10}$/',
+            'client_name' => 'required|regex:/^[\pL\s\-]+$/u|max:255',  // regex: allows for spaces and hyphens while also ensuring that the name only contains letters.
+            'client_number' => 'required|numeric|digits:10|regex:/^\d{10}$/',  // regex: value must be 10 digits & cannot contain non-numeric characters
             'client_email' => 'required | email |max:255|unique:clients',
             'status' => 'required | boolean',
         ];
