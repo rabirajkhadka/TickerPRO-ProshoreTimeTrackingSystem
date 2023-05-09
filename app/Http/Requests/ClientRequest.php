f460dccb890152a6391e742d8d49596eb24194e5
piyushmhrzn <piyushmaharjan22@gmail.com>
2023-05-09 22:59:21 +0545

    client number regex removed

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -24,10 +24,9 @@ class ClientRequest extends FormRequest
     public function rules()
     {
         return [
-            // name-regex: allows for spaces and hyphens while also ensuring that the name only contains letters.
+            // cleint_name-regex: allows for spaces and hyphens while also ensuring that the name only contains letters.
             'client_name' => 'required|regex:/^[\pL\s\-]+$/u|max:255',  
-            // number-regex:  it contains exactly 10 digits
-            'client_number' => 'required|numeric|digits:10|regex:/^\d{10}$/', 
+            'client_number' => 'required|numeric|digits:10', 
             'client_email' => 'required | email |max:255|unique:clients',
             'status' => 'required | boolean',
         ];
