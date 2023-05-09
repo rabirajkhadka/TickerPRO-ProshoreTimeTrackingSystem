eae4c17ebb77699f8246254d3b224b075b54d460
piyushmhrzn <piyushmaharjan22@gmail.com>
2023-05-09 14:29:40 +0545

    Updated requested changes

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -24,18 +24,13 @@ class ClientRequest extends FormRequest
     public function rules()
     {
         return [
-            'client_name' => 'required|regex:/^[\pL\s\-]+$/u|max:255',  // regex: allows for spaces and hyphens while also ensuring that the name only contains letters.
-            'client_number' => 'required|numeric|digits:10|regex:/^\d{10}$/',  // regex: value must be 10 digits & cannot contain non-numeric characters
+            // name-regex: allows for spaces and hyphens while also ensuring that the name only contains letters.
+            'client_name' => 'required|regex:/^[\pL\s\-]+$/u|max:255',  
+            // number-regex:  it contains exactly 10 digits
+            'client_number' => 'required|numeric|digits:10|regex:/^\d{10}$/', 
             'client_email' => 'required | email |max:255|unique:clients',
             'status' => 'required | boolean',
         ];
     }
 
-    public function messages()
-    {
-        return [
-            'client_number.numeric' => 'The client number must only contain numbers',
-            'client_number.digits' => 'The client number must be a 10 digit mobile number',
-        ];
-    }
 }
