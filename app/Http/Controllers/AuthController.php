3cdb10cf78ebcc800d024ebbe135bdd59dedb06c
smellycat5 <prajapatichunan@gmail.com>
2023-05-29 20:21:12 +0545

    removed minor errors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,24 +10,6 @@ use Mockery\Exception;
 
 class AuthController extends Controller
 {
-    public function registerUser(UserStoreRequest $request)
-    {
-        try {
-            $validatedUserRegister = $request->validated();
-            $user = UserService::saveUserData($validatedUserRegister);
-            $result = [
-                'status' => 200,
-                'user' => $user,
-            ];
-        } catch (Exception $e) {
-            $result = [
-                'status' => 403,
-                'message' => $e->getMessage()
-            ];
-        }
-        return response()->json($result, $result['status']);
-    }
-
     public function logoutUser(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
