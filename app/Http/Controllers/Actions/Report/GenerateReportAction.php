f0ea17ca9f855d911f68249106bd530a6857cd90
Amogh <Amoghshakya30@gmail.com>
2023-06-04 16:59:52 +0545

    folder report created

--- app/Http/Controllers/Actions/Report/GenerateReportAction.php
+++ app/Http/Controllers/Actions/Report/GenerateReportAction.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Actions\Report;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class GenerateReportAction extends Controller
