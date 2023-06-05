a5dad29ca51903c73d2df5f3ababfbe1505d228b
smellycat5 <prajapatichunan@gmail.com>
2023-06-05 16:01:16 +0545

    added total hours services

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -39,9 +39,6 @@ class ReportService
                 return [
                     'user_id' => $user->id,
                     'user_name' => $user->name,
-                    // 'project' => $validated['project_id'] !== null ?
-                    //     $user->timelogs->pluck('project.project_name')->first() :
-                    //     $user->timelogs->pluck('project.project_name')->unique(),
                     'client' => $user->timelogs->pluck('project.client.client_name')->first(),
                     'total_time' => intdiv($userTotalTime, 60) . 'hrs ' . ' ' . ($userTotalTime % 60) . 'min',
                     'activities' => $activites
@@ -73,45 +70,6 @@ class ReportService
                 $activity += ['project' => $timelog->project->project_name];
             return $activity;
         });
+        return $activities;
     }
 }
-
-
-// class ReportService2
-// {  
-//     public function totalhours($validated)
-//     {
-//     $reports = $users->map(function ($user) use ($validated) {
-//         $userTotalTime = $user->timelogs->sum(function ($timelog) {
-//             $startDateTime = Carbon::parse($timelog->start_date . ' ' . $timelog->started_time);
-//             $endDateTime = Carbon::parse($timelog->end_date . ' ' . $timelog->ended_time);
-//             return $endDateTime->diffInMinutes($startDateTime);
-//         });
-//         $activities = $user->timelogs->map(function ($timelog) use ($validated) {
-//             $startDateTime = Carbon::parse($timelog->start_date . ' ' . $timelog->started_time);
-//             $endDateTime = Carbon::parse($timelog->end_date . ' ' . $timelog->ended_time);
-//             $totalTime = $endDateTime->diffInMinutes($startDateTime);
-//             $activity = [
-//                 'activity' => $timelog->activity_name,
-//                 'total_time' =>  intdiv($totalTime, 60) . 'hrs ' . ' ' . ($totalTime % 60) . 'min',
-//                 'project' => $timelog->project->project_name
-//             ];
-//             if ($validated['project_id'] === null)
-//                 $activity += ['project' => $timelog->project->project_name];
-//             return $activity;
-//         });
-//         return [
-//             'user_id' => $user->id,
-//             'user_name' => $user->name,
-//             // 'project' => $validated['project_id'] !== null ?
-//             //     $user->timelogs->pluck('project.project_name')->first() :
-//             //     $user->timelogs->pluck('project.project_name')->unique(),
-//             'client' => $user->timelogs->pluck('project.client.client_name')->first(),
-//             'total_time' => intdiv($userTotalTime, 60) . 'hrs ' . ' ' . ($userTotalTime % 60) . 'min',
-//             'activities' => $activities,
-//         ];
-//     });
-//     // dd($reports);
-//     return ($reports->all());
-//     }
-// }
