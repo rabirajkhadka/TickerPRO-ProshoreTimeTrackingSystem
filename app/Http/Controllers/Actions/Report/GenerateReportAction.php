84e2cb48dc28b3f81cba1a38b3758a3e93f6ffca
Amogh <Amoghshakya30@gmail.com>
2023-06-05 15:05:11 +0545

    getUsersReport service created

--- app/Http/Controllers/Actions/Report/GenerateReportAction.php
+++ app/Http/Controllers/Actions/Report/GenerateReportAction.php
@@ -4,15 +4,38 @@ namespace App\Http\Controllers\Actions\Report;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TimelogReportRequest;
-use Illuminate\Http\Request;
+use App\Services\ReportService;
+use App\Traits\HttpResponses;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class GenerateReportAction extends Controller
 {
+    use HttpResponses;
+
+    protected ReportService $reportService;
+
+    /**
+     * @param ReportService $reportService
+     */
+    public function __construct(ReportService $reportService)
+    {
+        $this->reportService = $reportService;
+    }
+
+
     /**
      * @param TimelogReportRequest $request
      */
     public function __invoke(TimelogReportRequest $request)
     {
-        //
+        try {
+            $validated = $request->validated();
+            $report = $this->reportService->getUsersReport($validated);
+            return $this->successResponse([$report], 'Report successfully retrieved');
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            return $this->errorResponse([], "Something went wrong.");
+        }
     }
 }
--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -2,8 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Arr;
+
 class ReportService
 {
+    protected User $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
 
     /**
      * @param array $validated
@@ -11,7 +24,23 @@ class ReportService
      */
     public function getUsersReport(array $validated)
     {
-        //
+        $users = $this->user->whereIn('id', Arr::get($validated, 'user_id'))
+            ->with(['timelogs' => function ($query) use ($validated) {
+                $timelogQuery = $query->whereBetween('start_date', [Arr::get($validated, 'start_date'), Arr::get($validated, 'end_date')])
+                    ->whereBetween('end_date', [Arr::get($validated, 'start_date'), Arr::get($validated, 'end_date')])
+                    ->where('billable', 1)
+                    ->whereHas('project', function ($query) {
+                        $query->where('billable', 1);
+                    })
+                    ->with('project.client');
+
+                if ($validated['project_id'] !== null) {
+                    $timelogQuery->where('project_id', Arr::get($validated, 'project_id'));
+                }
+            }])->get();
+
+        $report = $this->getUsersReportDetails($validated, $users);
+        return $report;
     }
 
 
