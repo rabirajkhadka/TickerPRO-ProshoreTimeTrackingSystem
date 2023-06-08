21a77beb0f67fd7c726cd64be1f1d16a4b8e041b
Amogh <Amoghshakya30@gmail.com>
2023-06-08 17:03:10 +0545

    invite token verification made

---
+++ app/Http/Controllers/Actions/Auth/VerifyInviteTokenAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Actions\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ValidateInviteTokenRequest;
+use App\Traits\HttpResponses;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class VerifyInviteTokenAction extends Controller
+{
+    use HttpResponses;
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(ValidateInviteTokenRequest $request): JsonResponse
+    {
+        // dd($request);
+        return $this->successResponse([], 'Token verified');
+    }
+}
