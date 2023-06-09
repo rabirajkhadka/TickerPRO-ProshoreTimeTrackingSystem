bb3d1813f0497e42597884a3a8505ad33d237eff
Amogh <Amoghshakya30@gmail.com>
2023-06-09 11:58:06 +0545

    removed unwanted import

--- app/Http/Controllers/Actions/Auth/VerifyInviteTokenAction.php
+++ app/Http/Controllers/Actions/Auth/VerifyInviteTokenAction.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Actions\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ValidateInviteTokenRequest;
-use App\Traits\HttpResponses;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
